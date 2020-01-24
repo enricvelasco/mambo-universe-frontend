dc0cf415e00eaa74551300a11143cf7bef0cf5a4
enricvelasco <enric.velasco.g@gmail.com>
2020-01-24 14:14:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useContext, Suspense } from 'react'
+import { GlobalStyle } from './styles/GlobalStyles'
+
+const App = () => (
+  <div>
+    <h1>Hi this is React Starter!!</h1>
+    <p>This is an alternative to 'create-react-app'</p>
+    <p>remember that you owe this project has no style</p>
+    <p>we recommend that you use styled-components <a href='https://www.npmjs.com/package/styled-components' target='_blank' rel='noopener noreferrer'>https://www.npmjs.com/package/styled-components</a></p>
+  </div>
+);
+
+export default App
