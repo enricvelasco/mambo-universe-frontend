fbcba260892d3980bd1c55ca3d9fea3fe82fc34a
enric <enric.velasco.g@gmail.com>
2020-02-03 21:16:28 +0100

    first

---
+++ src/application/grid/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Column } from './style'
+
+const FlexboxGrid = () => {
+  return (
+    <Container>
+      GRID
+    </Container>
+  )
+}
+
+export default FlexboxGrid
---
+++ src/application/grid/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export const Row = styled.div`
+
+`
+
+export const Column = styled.div`
+
+`
