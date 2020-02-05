3f5ef6619fa7be796e7192529298328683ad4b7b
enricvelasco <enric.velasco.g@gmail.com>
2020-02-05 15:39:00 +0100

    include login outside path

---
+++ src/components/common/button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ButtonStyled } from './style'
+
+const Button = ({ children, onClick }) => (
+  <ButtonStyled onClick={onClick}>
+    {children}
+  </ButtonStyled>
+)
+
+export default Button
